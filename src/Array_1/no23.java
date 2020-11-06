3ea077fa809687b8f4fa2612af04c626d4c33fce
Apple <ellinemedrano1408@hotmail.com>
2020-11-06 19:50:42 +0100

    Updated

---
+++ src/Array_1/no23.java
@@ -0,0 +1,20 @@
+package Array_1;
+
+public class no23 {
+    //Given an int array length 2, return true if it does not contain a 2 or 3.
+    // no23([4, 5]) → true
+    //no23([4, 2]) → false
+    //no23([3, 5]) → false
+
+    public boolean no23(int[] nums)
+    {
+        if(nums[0] == 2 || nums[0] == 3)
+            return false;
+
+        if(nums[1] == 2 || nums[1] == 3)
+            return false;
+
+        return true;
+    }
+
+}
